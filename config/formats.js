2b582a59ab27798b3d90dcfca82aff1d596938f9
CTCC1 <twcnnj030@gmail.com>
2018-10-06 06:37:01 +0800

    update random formats

--- config/formats.js
+++ config/formats.js
@@ -48,7 +48,7 @@ let Formats = [
 		formatsList: [
 			//'gen2nu', 
 			'gen3uu', 'gen3pu', 'gen4uu', 'gen4lc', 'gen5pu', 
-			'gen5uu', 'gen7monotype', 'gen7monotype', 'gen7monotype'],
+			'gen5uu', 'gen7monotype'],
 		realFormat: '',
 		onBegin: function () {
 			this.add('html', `<div class="broadcast-green"><strong>CURRENT FORMAT: ` + this.realFormat + ` </strong></div>`);
