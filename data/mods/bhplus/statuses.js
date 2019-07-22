5ba940b7b43e8757c293169370b85fb9bbf06230
CTCC1 <twcnnj030@gmail.com>
2019-07-22 14:26:57 +0800

    Modernize bhplus status for TS

--- data/mods/bhplus/statuses.js
+++ data/mods/bhplus/statuses.js
@@ -1,15 +1,20 @@
-exports.BattleStatuses = {
-arceus: {
-onSwitchInPriority: 101,
-onSwitchIn: function (pokemon) {
-    var type = pokemon.types[0];
-    if (pokemon.ability === 'multitype') {
-        type = this.runEvent('Plate', pokemon);
-        if (!type || type === true) {
-            type = 'Normal'; //FOR THE MINDGAMES
-        }
-    }
-    pokemon.setType(type, true);
-}
-}
+'use strict';
+
+/**@type {{[k: string]: ModdedPureEffectData}} */
+let BattleStatuses = {
+	arceus: {
+		onSwitchInPriority: 101,
+		onSwitchIn(pokemon) {
+			let type = pokemon.types[0];
+			if (pokemon.ability === 'multitype') {
+				type = this.runEvent('Plate', pokemon);
+				if (!type) {
+					type = 'Normal'; //FOR THE MINDGAMES
+				}
+			}
+			pokemon.setType(type, true);
+		},
+	},
 };
+
+exports.BattleStatuses = BattleStatuses;
