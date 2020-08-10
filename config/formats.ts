e998f4674b36ae97b290cdededcc0d18b6145ba0
Starmie <978965142@qq.com>
2020-08-10 23:02:17 +0800

    merged with smogon

--- config/formats.ts
+++ config/formats.ts
@@ -552,7 +552,6 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 		],
 
 		mod: 'gen8',
-		searchShow: false,
 		ruleset: ['[Gen 8] National Dex'],
 		banlist: [
 			// National Dex OU
@@ -600,16 +599,17 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 			'Comatose + Sleep Talk',
 		],
 		onValidateSet(set) {
-			if (toID(set.ability) === 'intrepidsword' &&
-				!toID(set.species).startsWith('zacian') && toID(set.item) !== 'rustedsword') {
+			if (this.dex.toID(set.ability) === 'intrepidsword' &&
+				!this.dex.toID(set.species).startsWith('zacian') && this.dex.toID(set.item) !== 'rustedsword') {
 				return [`${set.ability} is banned.`];
 			}
-			if (set.species === 'Zacian-Crowned' && (toID(set.item) !== 'rustedsword' || toID(set.ability) !== 'intrepidsword')) {
+			if (set.species === 'Zacian-Crowned' &&
+				(this.dex.toID(set.item) !== 'rustedsword' || this.dex.toID(set.ability) !== 'intrepidsword')) {
 				return [set.species + " is banned."];
 			}
 		},
 		onChangeSet(set) {
-			const item = toID(set.item);
+			const item = this.dex.toID(set.item);
 			if (set.species === 'Zacian' && item === 'rustedsword') {
 				set.species = 'Zacian-Crowned';
 				set.ability = 'Intrepid Sword';
@@ -655,6 +655,7 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 		mod: 'megamax',
 		ruleset: ['[Gen 8] OU'],
 		banlist: ['Corviknight-Gmax', 'Melmetal-Gmax', 'Urshifu-Gmax'],
+		unbanlist: ['Cinderace'],
 		onChangeSet(set) {
 			if (set.species.endsWith('-Gmax')) set.species = set.species.slice(0, -5);
 		},
@@ -751,7 +752,7 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 			const god = target.side.team.find(set => {
 				let godSpecies = this.dex.getSpecies(set.species);
 				const validator = this.dex.getRuleTable(this.dex.getFormat(`gen${this.gen}ou`));
-				if (toID(set.ability) === 'powerconstruct' && this.gen === 7) {
+				if (this.toID(set.ability) === 'powerconstruct' && this.gen === 7) {
 					return true;
 				}
 				if (set.item) {
@@ -810,12 +811,13 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 			'Neutralizing Gas', 'Parental Bond', 'Protean', 'Pure Power', 'Shadow Tag', 'Stakeout', 'Water Bubble', 'Wonder Guard',
 		],
 		onValidateSet(set) {
-			if (set.species === 'Zacian-Crowned' && (toID(set.item) !== 'rustedsword' || toID(set.ability) !== 'intrepidsword')) {
+			if (set.species === 'Zacian-Crowned' &&
+				(this.dex.toID(set.item) !== 'rustedsword' || this.dex.toID(set.ability) !== 'intrepidsword')) {
 				return [set.species + " is banned."];
 			}
 		},
 		onChangeSet(set) {
-			const item = toID(set.item);
+			const item = this.dex.toID(set.item);
 			if (set.species === 'Zacian' && item === 'rustedsword') {
 				set.species = 'Zacian-Crowned';
 				set.ability = 'Intrepid Sword';
@@ -985,13 +987,13 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 			const names = new Set<ID>();
 			for (const set of team) {
 				const name = set.name;
-				if (names.has(toID(name))) {
+				if (names.has(this.dex.toID(name))) {
 					return [
 						`Your Pok\u00e9mon must have different nicknames.`,
 						`(You have more than one Pok\u00e9mon named '${name}')`,
 					];
 				}
-				names.add(toID(name));
+				names.add(this.dex.toID(name));
 			}
 			if (!names.size) {
 				return [
@@ -1130,7 +1132,7 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 					if (this.ruleTable.isRestrictedSpecies(pokemon)) continue;
 
 					for (const key of Object.values(pokemon.abilities)) {
-						const abilityId = toID(key);
+						const abilityId = this.dex.toID(key);
 						if (abilityId in teamHas.abilityMap) {
 							teamHas.abilityMap[abilityId][pokemon.evos ? 'push' : 'unshift'](speciesid);
 						} else {
@@ -1163,7 +1165,7 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 			this.format.debug = true;
 
 			if (!teamHas.abilitySources) teamHas.abilitySources = Object.create(null);
-			const validSources: string[] = teamHas.abilitySources[toID(set.species)] = []; // Evolution families
+			const validSources: string[] = teamHas.abilitySources[this.dex.toID(set.species)] = []; // Evolution families
 
 			let canonicalSource = ''; // Specific for the basic implementation of Donor Clause (see onValidateTeam).
 
@@ -1203,7 +1205,7 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 			// Donor Clause
 			const evoFamilyLists = [];
 			for (const set of team) {
-				const abilitySources = teamHas.abilitySources?.[toID(set.species)];
+				const abilitySources = teamHas.abilitySources?.[this.dex.toID(set.species)];
 				if (!abilitySources) continue;
 				let format = this.format;
 				if (!format.getEvoFamily) format = this.dex.getFormat('gen8inheritance');
@@ -1230,8 +1232,8 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 			for (const pokemon of this.getAllPokemon()) {
 				if (pokemon.baseAbility.includes('0')) {
 					const donor = pokemon.baseAbility.split('0')[1];
-					pokemon.m.donor = toID(donor);
-					pokemon.baseAbility = toID(pokemon.baseAbility.split('0')[0]);
+					pokemon.m.donor = this.toID(donor);
+					pokemon.baseAbility = this.toID(pokemon.baseAbility.split('0')[0]);
 					pokemon.ability = pokemon.baseAbility;
 				}
 			}
@@ -1279,7 +1281,7 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 			const sharedPower = new Set<string>();
 			for (const ally of pokemon.side.pokemon) {
 				if (ally.previouslySwitchedIn > 0) {
-					if (['mirrorarmor', 'trace'].includes(toID(ally.baseAbility))) continue;
+					if (['mirrorarmor', 'trace'].includes(ally.baseAbility)) continue;
 					sharedPower.add(ally.baseAbility);
 				}
 			}
@@ -1291,7 +1293,7 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 			if (!format.getSharedPower) format = this.dex.getFormat('gen8sharedpower');
 			for (const ability of format.getSharedPower!(pokemon)) {
 				const effect = 'ability:' + ability;
-				pokemon.volatiles[effect] = {id: toID(effect), target: pokemon};
+				pokemon.volatiles[effect] = {id: this.toID(effect), target: pokemon};
 			}
 		},
 		onSwitchInPriority: 2,
@@ -1320,7 +1322,7 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 			hasAbility(ability) {
 				if (this.ignoringAbility()) return false;
 				if (Array.isArray(ability)) return ability.some(abil => this.hasAbility(abil));
-				const abilityid = toID(ability);
+				const abilityid = this.battle.toID(ability);
 				return this.ability === abilityid || !!this.volatiles['ability:' + abilityid];
 			},
 		},
@@ -1350,7 +1352,7 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 				lcuber: 40,
 				lc: 40,
 			};
-			const tier = toID(species.tier) || 'ou';
+			const tier = this.toID(species.tier) || 'ou';
 			if (!(tier in boosts)) return;
 			const pokemon: Species = this.dex.deepClone(species);
 			const boost = boosts[tier];
@@ -1394,7 +1396,7 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 			const dex = this.dex;
 			const ability = dex.getMove(set.ability);
 			if (ability.category !== 'Status' || ability.status === 'slp' ||
-				this.ruleTable.isRestricted(`move:${ability.id}`) || set.moves.map(toID).includes(ability.id)) {
+				this.ruleTable.isRestricted(`move:${ability.id}`) || set.moves.map(this.dex.toID).includes(ability.id)) {
 				return this.validateSet(set, teamHas);
 			}
 			const customRules = this.format.customRules || [];
@@ -1416,7 +1418,7 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 		},
 		pokemon: {
 			getAbility() {
-				const move = this.battle.dex.getMove(toID(this.ability));
+				const move = this.battle.dex.getMove(this.battle.toID(this.ability));
 				if (!move.exists) return Object.getPrototypeOf(this).getAbility.call(this);
 				return {
 					id: move.id,
@@ -1654,7 +1656,7 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 		},
 		onSwitchInPriority: 100,
 		onSwitchIn(pokemon) {
-			let name: string = toID(pokemon.illusion ? pokemon.illusion.name : pokemon.name);
+			let name: string = this.toID(pokemon.illusion ? pokemon.illusion.name : pokemon.name);
 			if (this.dex.getSpecies(name).exists || name === 'rage') {
 				// Certain pokemon have volatiles named after their id
 				// To prevent overwriting those, and to prevent accidentaly leaking
--- server/users.ts
+++ server/users.ts
@@ -50,7 +50,7 @@ const MINUTES = 60 * 1000;
 const IDLE_TIMER = 60 * MINUTES;
 const STAFF_IDLE_TIMER = 30 * MINUTES;
 
-type StreamWorker = import('../lib/process-manager').StreamWorker;
+import type {StreamWorker} from '../lib/process-manager';
 
 /*********************************************************
  * Utility functions
@@ -326,6 +326,13 @@ export class Connection {
 
 type ChatQueueEntry = [string, RoomID, Connection];
 
+export interface UserSettings {
+	blockChallenges: boolean;
+	blockPMs: boolean | AuthLevel;
+	ignoreTickets: boolean;
+	hideBattlesFromTrainerCard: boolean;
+}
+
 // User
 export class User extends Chat.MessageContext {
 	readonly user: User;
@@ -363,12 +370,7 @@ export class User extends Chat.MessageContext {
 	lastMatch: string;
 	forcedPublic: string | null;
 
-	settings: {
-		blockChallenges: boolean,
-		blockPMs: boolean | AuthLevel,
-		ignoreTickets: boolean,
-		hideBattlesFromTrainerCard: boolean,
-	};
+	settings: UserSettings;
 
 	battleSettings: {
 		team: string,
