ae2afeafdabd7269daf4a59cc271dbc013b7fa25
Starmie <978965142@qq.com>
2021-02-11 19:36:35 +0800

    Spring festival activity: Durants

--- data/random-teams.ts
+++ data/random-teams.ts
@@ -3491,36 +3491,49 @@ export class RandomTeams {
 	randomDurantsTeam() {
 		const pokemon = [];
 		let names = FS('config/durant-names.txt').readSync('utf8').split(',');
+		const species = this.dex.getSpecies('Durant');
 		while (pokemon.length < 6) {
-			const species = this.dex.getSpecies('Durant');
 			const set = {
 				name: this.sample(names),
 				species: species.name,
 				gender: species.gender,
 				item: this.random(2) < 1 ? 'Choice Scarf' : 'Leppa Berry',
-				ability: 'Swarm',
+				ability: 'Hustle',
 				shiny: false,
 				evs: {hp: 4, atk: 252, def: 0, spa: 0, spd: 0, spe: 252},
 				nature: 'Jolly',
 				ivs: {hp: 31, atk: 31, def: 31, spa: 31, spd: 31, spe: 31},
 				moves: ['Guillotine'],
 			};
-			if (this.random(48) < 1) {
-				set.item = 'Watmel Berry';
-				set.moves.push('Natural Gift');
-				set.shiny = true;
-			} else if (this.random(96) < 1) {
+			if (this.random(4) < 1) {
+				set.moves.push('Spite');
+			} else if (this.random(6) < 1) {
+				set.ability = 'Cursed Body';
+			} else if (this.random(8) < 1) {
 				set.moves.push('Superpower');
-				set.shiny = true;
-			} else if (this.random(144) < 1) {
+			} else if (this.random(10) < 1) {
+				set.moves.push('Foresight');
+				set.item = 'Bright Powder'
+			} else if (this.random(12) < 1) {
 				set.item = 'Focus Sash';
-				set.shiny = true;
-			} else if (this.random(192) < 1) {
+			} else if (this.random(14) < 1) {
+				set.moves.push('Assist');
+				set.ability = ('Prankster');
+			} else if (this.random(16) < 1) {
+				set.moves.push('Dynamic Punch');
+			} else if (this.random(18) < 1) {
 				set.item = 'Leppa Berry';
 				set.moves.push('Imprison');
-				set.shiny = true;
 			}
 			pokemon.push(set);
+		}
+		pokemon[0].shiny = true;
+		if (this.random(4) < 1) {
+			pokemon[0].moves.push('Inferno');
+			pokemon[0].ability = 'Truant';
+		} else {
+			pokemon[0].item = 'Watmel Berry';
+			pokemon[0].moves.push('Natural Gift');
 		}
 		return pokemon;
 	}
