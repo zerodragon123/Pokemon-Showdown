b347b24a53c3650368ad25d96736a8a7fc894edf
Starmie <978965142@qq.com>
2021-06-12 22:43:43 +0800

    Pet mode v1.0

---
+++ config/pet-mode/gym-config.js
@@ -0,0 +1,53 @@
+exports.PetModeGymConfig = {
+    "苍蓝": {
+        "maxlevel": 10,
+        "userteam": "No Evasion Moves",
+        "botteam": "Farfetch'd||Stick|Defiant|LeafBlade,Slash|Bashful|,0,36,,,0|M|||10|]Mantyke||Wacan Berry|WaterAbsorb|BubbleBeam,AirCutter,Roost|Bold|76,0,100,0,20,0|M|||10|]Gligar||RockyHelmet|Immunity|Earthquake,Roost,KnockOff,Toxic|Impish|0,80,0,0,40,0|M|||10|250]Oricorio-Sensu||Metronome|Dancer|OminousWind,AirCutter|Timid|,,,,,||||10|",
+        "ace": "Oricorio-Sensu",
+	    "msg": {
+            "start": "欢迎来到，新人训练家都要经过的第一站——苍蓝道馆。来吧，让我们一起在苍蓝的天空起舞吧。",
+            "ace": "要进入高潮咯，希望你能跟上我的步伐",
+            "win": "真是一场美妙的舞会，没想到你作为新人训练家竟如此出色。这枚苍蓝徽章是你应得的，收下他吧。",
+            "lose": "学会飞行最难的就是第一次振翅的勇气，请再接再厉，这片天空属于未来的你们！"
+        }
+    },
+    "湛蓝": {
+        "maxlevel": 20,
+        "userteam": "No Evasion Moves, No Repeat Pokemon",
+        "botteam": "Lanturn||Rindo Berry|VoltAbsorb|Scald,Thunderbolt,VoltSwitch,IceBeam|Calm|252,0,4,0,252,0|M|||20|]Keldeo-Resolute||Expert Belt|Justified|Surf,SecretSword,Air Slash|Timid|,,,252,,252||||20|]Qwilfish||BlackSludge|Intimidate|Waterfall,Poison Jab|Impish|236,0,196,0,76,0|M|||20|]Kingler||LifeOrb|SheerForce|Liquidation,X-Scissor,RockSlide|Adamant|236,176,0,0,76,0|M|||20|250",
+        "ace": "Keldeo-Resolute",
+	    "msg": {
+            "start": "经历过天空洗礼的你，能否经得住波涛的汹涌呢？",
+            "ace": "决心。。。你的决心又是什么呢？",
+            "win": "天空，海洋，下一个就是冰雪。。。。没什么，自言自语罢了。收下吧，这是湛蓝徽章，曾经我也是这么一步步走来的，希望有一天你也能看到山顶的风景。",
+            "lose": "海洋固然广阔，但只要你愿意扬帆，不畏波涛，就一定能驶向远方，请再接再厉。"
+        }
+    },
+    "冰蓝": {
+        "maxlevel": 30,
+        "userteam": "No Evasion Moves, No Repeat Pokemon",
+        "botteam": "Vanilluxe||LightClay|SnowWarning|Blizzard,Freeze-Dry,AuroraVeil|Timid|0,0,0,252,0,252|M|||30|]Mamoswine||Choice Band|Thick Fat|Icicle Spear|Jolly|252,252,252,,252,252|M|||30|250]Arctozolt|||SlushRush|BoltBeak,Icicle Crash|Adamant|,252,,,,252|F|||30|]Abomasnow||Abomasite|Soundproof|Blizzard,GigaDrain,JungleHealing|Modest|252,252,252,252,252,|F|||30|]Articuno||Metronome|MagicGuard|Blizzard,BraveBird,Roost|Jolly|252,252,252,252,252,252|F|||30|",
+        "weather": "hail",
+        "ace": "Articuno",
+	    "msg": {
+            "start": "这里是蓝色三馆的最后一站。在雪山当中最致命就是无尽的暴风雪与冰雹，但愿你能冲破这重重的险阻。",
+            "ace": "曾经我也是为他所救的一人，也是在这次建立起了我们的羁绊。",
+            "win": "飞跃苍穹，不畏浩瀚，勇登高峰，这样的你已经完全是一个合格的训练师了。加油，更广阔的的天地在等待着你。",
+            "lose": "暴风雪是最容易让人迷失方向的地方，但我相信只要你心中有着指引你的方向就一定不会迷茫，希望下次能让我看到更加强大的你。"
+        }
+    },
+    "坚毅": {
+        "maxlevel": 40,
+        "userteam": "No Evasion Moves, No Repeat Pokemon, No Baton Pass",
+        "botteam": "Duraludon||Chople Berry|Teravolt|FlashCannon,Dragon Pulse|Timid|252,,252,252,252,252||,0,,,,||43|]Kartana||assaultvest|waterbubble|leafblade,sunsteelstrike,spectralthief|Jolly|252,252,252,,252,252||||43|]Bisharp||LifeOrb|Defiant|IronHead,KnockOff,SuckerPunch|Jolly|252,252,252,252,252,252||,0,,,,||43|]Celesteela||assaultvest|thickfat|heavyslam,aeroblast,flamethrower|Relaxed|252,252,252,252,252,252||||43|]Metagross||metagrossite|ClearBody|sunsteelstrike,psychicfangs|Jolly|252,252,252,,252,252||||43|]Melmetal||assaultvest|unaware|DoubleIronBash,Sunsteel Strike,Spectral Thief|Brave|252,252,252,,252,||||43|",
+        "terrain": "steelterrain",
+        "ace": "Metagross",
+	    "msg": {
+            "start": "这里是勇者修行的第一站，与之前相比，这里的修行恐怕会更加的令人绝望。穿上这件磁力服，在这里，坚定的意志将使属性克制不再那么重要，不过如果想要偷懒回复的话，可要做好被场地电击打断的准备。",
+            "ace": "这是最后的试炼。来吧！让我看看你修行的成果。",
+            "win": "这是坚毅的证明，希望你和你的宝可梦能记住这段时间修行的成果。顺便帮我向那个老太婆打个招呼，可别被她的小伎俩给蒙住了眼睛。",
+            "lose": "小子，连这第一关都过不去，还想成为顶尖的训练师？"
+        },
+        "bonus": "box"
+    }
+}
