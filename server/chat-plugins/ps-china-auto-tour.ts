a30110e0560a068d124799fde9b69a0b9849ca38
Starmie <978965142@qq.com>
2022-01-25 00:39:01 +0800

    Team Database

--- server/chat-plugins/ps-china-auto-tour.ts
+++ server/chat-plugins/ps-china-auto-tour.ts
@@ -20,7 +20,8 @@ type TourTiming = {
 type TourSettings = {
 	format: string,
 	rules: TourRules,
-	timing: TourTiming
+	timing: TourTiming,
+	desc?: string,
 };
 
 type TourStatus = {
@@ -112,17 +113,15 @@ class ScoreTourUtils {
 		}
 		return result;
 	}
-	static addTourScore(tourname: string, tourLog: AnyObject) {
+	static async addTourScore(tourname: string, tourLog: AnyObject) {
 		try {
-			Object.keys(tourLog).forEach((userId, i) => {
+			for (let userId of Object.keys(tourLog)) {
 				const wins = tourLog[userId].length;
 				let score = SCORE_BONUS[wins] || 0;
 				if (score > 0) {
-					setTimeout(() => {
-						AdminUtils.addScoreToMain(userId, score, `{}在 ${tourname} 淘汰赛中连胜 ${wins} 轮`);
-					}, 100 * i);
+					await AdminUtils.addScoreToMain(userId, score, `{}在 ${tourname} 淘汰赛中连胜 ${wins} 轮`);
 				}
-			})
+			}
 		} catch (err) {
 			Rooms.get('staff')?.add(`|c|&|/log ${tourname} 淘汰赛自动加分失败`).update();
 		}
@@ -227,6 +226,9 @@ class TourQueue {
 				}
 			}
 			broadcastContext.sendReply(TourQueue.formatInfo(format));
+			if (tourStatus.settings.desc) {
+				broadcastContext.sendReply(tourStatus.settings.desc);
+			}
 		} else {
 			broadcastContext.errorReply('Failed to create a tournament.');
 		}
@@ -361,6 +363,7 @@ export const commands: Chat.ChatCommands = {
 				const roomid = room!.roomid;
 				const canEdit = Users.Auth.hasPermission(user, 'roommod', null, room);
 				let buf = '|uhtml|auto-tour-config|';
+				let notSaved = !!tmpTourConfig[user.id];
 				const roomTourConfig = tmpTourConfig[user.id] || tourConfig[roomid] || [];
 				if (roomTourConfig.length) {
 					buf += '<table style="border-spacing: 5px;">';
@@ -388,14 +391,17 @@ export const commands: Chat.ChatCommands = {
 						let row = [formatName, timing, buttons];
 						buf += '<tr>' + row.map(s => `<td style="text-align: center">${s}</td>`).join('') + '</tr>';
 					});
+					if (canEdit) {
+						let row = [button(`/autotour config edit ${roomTourConfig.length}`, 'Add'), '', ''];
+						buf += '<tr>' + row.map(s => `<td style="text-align: center">${s}</td>`).join('') + '</tr>';
+					}
 					buf += '</table>';
 				} else {
 					buf += '<p>There is no auto tour configured in this room.</p>';
 				}
-				if (canEdit) {
+				if (notSaved) {
 					buf += '<p>';
-					buf += button(`/autotour config edit ${roomTourConfig.length}`, 'Add');
-					buf += button(`/autotour config save`, 'Confirm');
+					buf += button(`/autotour config save`, 'Confirm & Save');
 					buf += button(`/autotour config cancel`, 'Cancel');
 					buf += '</p>';
 				}
@@ -444,6 +450,10 @@ export const commands: Chat.ChatCommands = {
 					} else {
 						lines.push('Inactive players will not be automatically disqualified.');
 					}
+					if (roomTourConfig[index].desc) {
+						lines.push(`<b>Description:</b>`);
+						lines.push(roomTourConfig[index].desc);
+					}
 					lines.push(button('/autotour config', 'Back'));
 					return this.sendReply(`|uhtml|auto-tour-config|${lines.join('<br/>')}`);
 				}
@@ -462,6 +472,7 @@ export const commands: Chat.ChatCommands = {
 					delete tmpTourConfig[user.id];
 				}
 				this.sendReply('|uhtml|auto-tour-config|');
+				this.parse('/autotour config');
 				this.sendReply('Auto tour config updated.');
 			},
 			cancel(target, room, user) {
@@ -469,6 +480,7 @@ export const commands: Chat.ChatCommands = {
 				this.checkCan('roommod', null, room!);
 				delete tmpTourConfig[user.id];
 				this.sendReply('|uhtml|auto-tour-config|');
+				this.parse('/autotour config');
 			},
 			edit(target, room, user) {
 				this.requireRoom();
@@ -485,6 +497,9 @@ export const commands: Chat.ChatCommands = {
 						case 'delete':
 							tmpTourConfig[user.id].splice(index, 1);
 							return this.parse('/autotour config');
+						case 'desc':
+							tourSettings.desc = target.split('desc')[1].slice(1);
+							return this.parse(`/autotour config edit ${index}`);
 						case 'format':
 							const format = Dex.formats.get(args);
 							if (format.exists) {
@@ -570,6 +585,11 @@ export const commands: Chat.ChatCommands = {
 							buf += `<b>Timing: Minutes</b><br/>`;
 							buf += `<form data-submitsend="${cmdPrefix},minutes,{autotour-minutes}">`;
 							buf += `<input name="autotour-minutes" placeholder="${tourSettings.timing.minutes}" style="width: 200px"/>`;
+							buf += `<button class="button" type="submit">OK</button>`;
+							buf += `</form>`;
+							buf += `<b>Description (Optional):</b> ${tourSettings.desc}<br/>`;
+							buf += `<form data-submitsend="${cmdPrefix},desc,{autotour-desc}">`;
+							buf += `<input name="autotour-desc" placeholder="${Utils.escapeHTML(tourSettings.desc || 'undefined')}" style="width: 200px"/>`;
 							buf += `<button class="button" type="submit">OK</button>`;
 							buf += `</form>`;
 							buf += button(`/autotour config`, 'Confirm & Back');
---
+++ server/chat-plugins/ps-china-team-database.ts
@@ -0,0 +1,226 @@
+import { FS } from "../../lib";
+import { NetRequest } from "../../lib/net";
+import { escapeHTML } from "../../lib/utils";
+import { PetUtils } from "./ps-china-pet-mode";
+
+const TEAM_DATABASE_DIR = 'config/ps-china/team-db/gen8ou';
+const SIGNIFICANT_POKES_FILE = `${TEAM_DATABASE_DIR}/pokes.json`;
+const REPLAY_URLS_FILE = `${TEAM_DATABASE_DIR}/replay-urls.txt`;
+const TEAM_DATABASE_FILE = `${TEAM_DATABASE_DIR}/teams.json`;
+
+if (!FS(TEAM_DATABASE_DIR).existsSync()) FS(TEAM_DATABASE_DIR).mkdirpSync();
+if (!FS(TEAM_DATABASE_FILE).existsSync()) FS(TEAM_DATABASE_FILE).writeSync('[]');
+
+let teamDB: {
+	"teamCode": number[],
+	"players": [{"playerId": string, "replays": string[]}]
+}[] = JSON.parse(FS(TEAM_DATABASE_FILE).readIfExistsSync());
+
+let pokeNum = 0;
+let pokeIndex: {[speciesId: string]: number} = {};
+if (FS(SIGNIFICANT_POKES_FILE).existsSync()) {
+	JSON.parse(FS(SIGNIFICANT_POKES_FILE).readIfExistsSync()).forEach((speciesId: string) => {
+		pokeIndex[speciesId] = pokeNum;
+		pokeNum += 1;
+	})
+} else {
+	Dex.species.all().forEach((species) => {
+		if (['OU', 'UUBL', 'UU', 'RUBL', 'RU'].includes(species.tier) || ['urshifu', 'kyurem'].includes(species.id)) {
+			pokeIndex[species.id] = pokeNum;
+			pokeNum += 1;
+		}
+	});
+	FS(SIGNIFICANT_POKES_FILE).writeSync(JSON.stringify(Object.keys(pokeIndex)));
+}
+const teamCodeLength = Math.ceil(pokeNum / 31);
+
+function getTeamCode(team: string[]): number[] {
+	let teamCode: number[] = new Array(teamCodeLength).fill(0);
+	for (let speciesId of team) {
+		if (pokeIndex[speciesId] !== undefined) {
+			teamCode[Math.floor(pokeIndex[speciesId] / 31)] += 1 << (pokeIndex[speciesId] % 31);
+		}
+	}
+	return teamCode;
+}
+
+function findSimilarTeams(targetTeamCode: number[]): number[][] {
+	const similarTeams: number[][] = [[], [], []];
+	for (let i = 0; i < teamDB.length; i++) {
+		const teamCode = teamDB[i]['teamCode'];
+		let simVal = 0;
+		for (let j = 0; j < teamCodeLength; j++) {
+			let x = teamCode[j] & targetTeamCode[j];
+			while (x > 0) {
+				simVal += x % 2;
+				x = x >> 1;
+			}
+		}
+		if (simVal >= 4) {
+			similarTeams[simVal - 4].push(i);
+		}
+	}
+	return similarTeams;
+}
+
+function findTeamIndex(targetTeamCode: number[]): number {
+	for (let i = 0; i < teamDB.length; i++) {
+		const teamCode = teamDB[i]['teamCode'];
+		let sameCode = true;
+		for (let j = 0; j < teamCodeLength; j++) {
+			if (teamCode[j] ^ targetTeamCode[j]) {
+				sameCode = false;
+				break;
+			}
+		}
+		if (sameCode) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+async function updateTeamDB(replayUrl: string): Promise<boolean> {
+	try {
+		const replayStr = await new NetRequest(replayUrl).get();
+		for (let side of ['p1', 'p2']) {
+			let s = replayStr.split(`|player|${side}|`).slice(1)[0];
+			const playerId = toID(eval(`'${s.slice(0, s.indexOf('|'))}'`));
+			const team = []
+			for (let s of replayStr.split(`|poke|${side}|`).slice(1)) {
+				let speciesId = toID(eval(`'${s.slice(0, s.indexOf('|')).split(',')[0]}'`));
+				team.push(speciesId);
+			}
+			const teamCode = getTeamCode(team);
+			const teamIndex = findTeamIndex(teamCode);
+			if (teamIndex < 0) {
+				teamDB.push({
+					'teamCode': teamCode,
+					'players': [{
+						'playerId': playerId,
+						'replays': [replayUrl]
+					}]
+				});
+			} else {
+				let newPlayer = true;
+				for (let playerIndex = 0; playerIndex < teamDB[teamIndex]['players'].length; playerIndex++) {
+					if (teamDB[teamIndex]['players'][playerIndex]['playerId'] === playerId) {
+						teamDB[teamIndex]['players'][playerIndex]['replays'].push(replayUrl);
+						newPlayer = false;
+						break;
+					}
+				}
+				if (newPlayer) {
+					teamDB[teamIndex]['players'].push({
+						'playerId': playerId,
+						'replays': [replayUrl]
+					});
+				}
+			}
+		}
+	} catch (err) {
+		return false;
+	}
+	return true;
+}
+
+async function saveTeamDB(): Promise<boolean> {
+	try {
+		FS(TEAM_DATABASE_FILE).writeSync(JSON.stringify(teamDB, null, '\t'));
+	} catch (err) {
+		return false;
+	}
+	return true;
+}
+
+function codeToTeam(teamCode: number[]): string[] {
+	const team = [];
+	const pokes = Object.keys(pokeIndex);
+	for (let i = 0; i < teamCodeLength; i++) {
+		let x = teamCode[i];
+		for (let j = 0; x > 0; x >>= 1, j++) {
+			if (x % 2) {
+				team.push(pokes[i * 31 + j]);
+			}
+		}
+	}
+	return team;
+}
+
+function showTeam(team: string[]): string {
+	return team.map(speciesId => `<psicon pokemon="${speciesId}"/>`).join('');
+}
+
+export const commands: Chat.ChatCommands = {
+	tdbs(target, room, user) {
+		this.parse(`/teamdb search ${target}`);
+	},
+	teamdb: {
+		async update(target, room, user) {
+			this.requireRoom();
+			this.checkCan('lockdown');
+			if (room!.roomid !== 'wcop') return this.errorReply('Access denied.');
+			if (!FS(REPLAY_URLS_FILE).existsSync()) {
+				this.errorReply('Replay URL file not found.');
+			} else {
+				const replayUrls = FS(REPLAY_URLS_FILE).readIfExistsSync().replace(/\r/g, '').split('\n');
+				for (let i = 0; i < replayUrls.length; i++) {
+					user.sendTo(room!.roomid, `|uhtml|teamdb-update|Loading ${i} / ${replayUrls.length}`);
+					const success = await updateTeamDB(replayUrls[i]);
+					if (!success) this.errorReply(`Failed to load replay: ${replayUrls[i]}`);
+					if (i % 100 === 0) await saveTeamDB(); // TODO: remove this line
+				}
+				const success = await saveTeamDB();
+				if (success) {
+					FS(REPLAY_URLS_FILE).unlinkIfExistsSync();
+					user.sendTo(room!.roomid, `|uhtmlchange|teamdb-update|`);
+					this.sendReply(`Finished. Replay URL file deleted.`);
+				} else {
+					this.errorReply(`Failed to save team database.`);
+				}
+			}
+		},
+		async search(target, room, user) {
+			this.requireRoom();
+			if (room!.roomid !== 'wcop') return this.errorReply('Access denied.');
+			target = target.replace(/,/g, '/');
+			const args = target.split('/').map(toID);
+			if (args.length !== 6 || args.some(x => !Dex.species.get(x).exists)) {
+				return this.errorReply('Please input 6 legal species.');
+			}
+			let buf = `<p>Teams similar to ${showTeam(args)}</p>`;
+			const similarTeams = findSimilarTeams(getTeamCode(args));
+			for (let i = 2; i >= 0; i--) {
+				buf += `<details title="Click to view teams">`;
+				buf += `<summary>${i + 4} hits: ${similarTeams[i].length} team${similarTeams[i][1] ? 's' : ''}</summary>`;
+				if (similarTeams[i].length > 0) {
+					similarTeams[i].forEach(teamIndex => {
+						const teamInfo = teamDB[teamIndex];
+						buf += `<details title="Click to view replays" style="left: 20px; position: relative">`;
+						buf += `<summary>${showTeam(codeToTeam(teamInfo['teamCode']))}</summary>`;
+						let replayTable: string[][] = [];
+						teamInfo['players'].forEach(playerInfo => {
+							let scoutUrl = 'https://fulllifegames.com/Tools/ReplayScouter/?';
+							scoutUrl += `name=${playerInfo['playerId']}&tier=&opponent=&`;
+							scoutUrl += `replays=${playerInfo['replays'].map(s => escapeHTML(s.replace('.json', ''))).join('%0D%0A')}`;
+							const playerBar =`<a href="${scoutUrl}">${playerInfo['playerId']}</a>`;
+							playerInfo['replays'].forEach((replayUrl, i) => {
+								const readableUrl = replayUrl.replace('.json', '');
+								replayTable.push([
+									i === 0 ? playerBar : '',
+									`<a href="${readableUrl}">${readableUrl}</a>`
+								]);
+							});
+						});
+						buf += PetUtils.table([], ['Player', 'Replay'], replayTable);
+						buf += `</details>`;
+					});
+				} else {
+					buf += `<p>Not found.</p>`;
+				}
+				buf += `</details>`;
+			}
+			user.sendTo(room!.roomid, `|uhtml|teamdb-search|${buf}`);
+		}
+	}
+}
