366e5a9925bac8b678ebdf2db6227e20fa28ff77
Starmie <978965142@qq.com>
2022-01-25 00:39:01 +0800

    Team Database

--- config/pet-mode/lottery-config.js
+++ config/pet-mode/lottery-config.js
@@ -1,6 +1,6 @@
 exports.PetModeLotteryConfig = {
-	"start": "2022-01-10",
-	"end": "2022-01-16",
+	"start": "2022-03-24",
+	"end": "2022-03-30",
 	"host": [
 		"petlottery",
 		"xpsh",
@@ -9,12 +9,12 @@ exports.PetModeLotteryConfig = {
 	],
 	"live": "https://live.bilibili.com/21180065",
 	"awards": {
-		"1v": "Dome Fossil, 5,item",
-		"2v": "{},S,6V,egg",
-		"3v": "Terrakion,4V,egg",
-		"4v": "Terrakion,6V,S,egg",
-		"5v": "Zacian,6V,egg",
-		"6v": "Arceus,6V,egg"
+		"1v": "已结束,1",
+		"2v": "",
+		"3v": "敬请,1",
+		"4v": "期待,1",
+		"5v": "第二季,1",
+		"6v": "加量不加价,1"
 	},
 	"price": 5
 }
